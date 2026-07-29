3cb89275a658893a248fc610cd02b31a9623518d
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-07-29 19:51:02 +0530

    first

---
+++ MadlibsGame.py
@@ -0,0 +1,9 @@
+adjective1=input("Enter adjective1 ")
+noun1=input("Enter noun1 ")
+verb1=input("Enter verb1 ")
+adjective2=input("Enter adjective2 ")
+adjective3=input("Enter adjective3 ")
+
+print(f"Today I went to {adjective1} zoo")
+print(f"I saw a {noun1} jumping up and down in its tree")
+print(f"He {verb1} through the large tunnel that led to its {adjective2} {adjective3}")
