33c576308a4a0f3272ed87ca611d13de5195697f
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-08-02 19:47:08 +0530

    random

---
+++ randomPractice.py
@@ -0,0 +1,15 @@
+import random
+
+low=90
+high=100
+options=("rock","paper","scissors")
+
+option=random.choice(options)
+
+#print(f"You rolled a {option}!")
+
+card=["king","queen","jack","ace","2","3","4","5","6","7","8","9","10"]
+
+random.shuffle(card)
+#print(card)
+
