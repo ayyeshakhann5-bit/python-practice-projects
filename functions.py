995d4117af2a74472b188f5551177ec696786e4a
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-08-06 23:59:06 +0530

    function

--- functions.py
+++ functions.py
@@ -1,1 +1,7 @@
 #a function is a block of code
+
+def dispaly_invoice(username,amount,due_date):
+  print(f"Hello {username}")
+  print(f"your bill {amount:.2f} is due date for {due_date}")
+
+dispaly_invoice("Ayesha",34.789,"01/08")
