fa9bcdc12869b949eafb6f8a0e2d0634ee64a709
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-08-22 19:58:14 +0530

    Operators

---
+++ MembershipOperators.py
@@ -0,0 +1,24 @@
+
+"""
+word="APPLE"
+
+Guess=input("Guess any letter:")
+
+if Guess in word:
+  print("present")
+
+else:
+  print("not")
+
+  """
+
+students={"Aisha","khan"}
+
+student=input("Enter name of student:")
+
+if student not in students:
+  print(f"{student} was not found")    
+
+else:
+  print(f"{student} is a student")  
+
