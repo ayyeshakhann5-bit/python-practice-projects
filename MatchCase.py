eef9caa1e7f5a95b3ec9360b8bb2131ff48ed2be
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-08-27 00:58:12 +0530

    Match

--- MatchCase.py
+++ MatchCase.py
@@ -1,3 +1,5 @@
+"""
+
 def week_days(day):
     match day:
         case 1:
@@ -7,6 +9,18 @@ def week_days(day):
         case _:
             return "Not a valid day"
           
-print(week_days(1))    
+print(week_days("pizza"))    
+
+"""
 
 
+def is_weekend(day):
+    match day:
+      case "Saturday" | "Sunday":
+        return True
+      case "Monday" | "Tuesday":
+        return False
+      case _:
+        return False
+      
+print(is_weekend("Saturday"))      
