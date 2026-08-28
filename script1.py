7646d6c8573f4b7cf9778871e2cb510d3a14fc87
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-08-28 23:17:52 +0530

    main

---
+++ script1.py
@@ -0,0 +1,13 @@
+def favorite_food(food):
+  print(f"Your favorite food is {food}")
+  
+print("This is script1")
+favorite_food("Pizza")
+print("Good bye")
+  
+  
+"""
+if __name__=='__main__':
+  main() 
+"""
+  
