407df22485de236056639c780d37eb7ccaf42085
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-08-30 19:15:16 +0530

    Object

--- ObjectCall.py
+++ ObjectCall.py
@@ -4,3 +4,23 @@ class Car:
     self.year=year
     self.color=color
     self.for_sale=for_sale
+    
+  """  
+  def drive(self):
+    print("You drive the car")
+    
+  def stop(self):
+    print("You stop the car")      
+    
+  """  
+  
+  def drive(self):
+    print(f"You drive the {self.model}")
+    
+  def stop(self):
+    print(f"You stop the {self.model}")        
+    
+    
+  def describe(self):
+    print(f"{self.year} {self.color} {self.for_sale}")    
+    
--- object.py
+++ object.py
@@ -4,6 +4,8 @@ from ObjectCall import Car
 car1=Car("Mustang",2024,"red",False)
 car2=Car("Corvette",2025,"blue",True)
     
+ 
+"""
     
 print(car1.model)    
 print(car1.year)
@@ -14,3 +16,17 @@ print(car2.model)
 print(car2.year)
 print(car2.color)
 print(car2.for_sale)
+
+ """
+"""
+car1.drive()
+car1.stop()
+"""
+
+"""
+car1.drive()
+car1.stop()
+"""
+
+car2.describe()
+
