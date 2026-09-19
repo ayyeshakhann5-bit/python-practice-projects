90e8cbf5c24bc62c42c95795e1f94051f27e3f7e
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-09-19 16:04:36 +0530

    WritingFiles

--- writingFiles.py
+++ writingFiles.py
@@ -1,7 +1,26 @@
 #python writing files(.txt,.jsn,.csv)
 
+import json
 
-employees=["Ayesha","khan","Aisha","khannn"]
+import csv
+
+"""
+employee={
+  "name":"Ayesha",
+  "age":24,
+  "Company":"Servicenow"
+  
+}
+
+"""
+
+employees=[["Name","Age","Job"],
+           ["Ayesha",24,"Technical Engineer"],
+           ["Khan",25,"SDE"]]
+
+
+
+#employees=["Ayesha","khan","Aisha","khannn"]
 
 #file_path='output.txt'
 file_path='/Users/ayesha.khan/Desktop/output.txt'
@@ -10,9 +29,11 @@ try:
     with open(file_path, "w") as file:
     #with open(file_path, "x") as file:
     #with open(file_path, "a") as file:
-         for employee in employees:
-           file.write(employee+" ")
-         print(f"txt file {file_path} is created")
+         #json.dump(employee,file,indent=5)
+         writer=csv.writer(file)
+         for row in employees:
+           writer.writerow(row)
+         print(f"CSV file {file_path} is created")
          
 except FileExistsError:
   print("That file already exists")         
