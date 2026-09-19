ac84fc603d66e7a2120fbbeb90845e46f1606487
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-09-19 15:36:17 +0530

    writingFiles

--- writingFiles.py
+++ writingFiles.py
@@ -1,13 +1,21 @@
 #python writing files(.txt,.jsn,.csv)
 
 
-txt_data='I like Pizza'
+employees=["Ayesha","khan","Aisha","khannn"]
 
 #file_path='output.txt'
 file_path='/Users/ayesha.khan/Desktop/output.txt'
 
-with open(file_path, "w") as file:
-     file.write(txt_data)
-     print(f"txt file {file_path} is created")
+try:
+    with open(file_path, "w") as file:
+    #with open(file_path, "x") as file:
+    #with open(file_path, "a") as file:
+         for employee in employees:
+           file.write(employee+" ")
+         print(f"txt file {file_path} is created")
+         
+except FileExistsError:
+  print("That file already exists")         
+         
   
 
