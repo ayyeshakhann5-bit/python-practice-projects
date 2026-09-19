8de8517c0a7953f85c145acf89606521895e0c30
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-09-19 15:11:51 +0530

    writingFiles

---
+++ writingFiles.py
@@ -0,0 +1,12 @@
+#python writing files(.txt,.jsn,.csv)
+
+
+txt_data='I like Pizza'
+
+file_path='output.txt'
+
+with open(file_path, "w") as file:
+     file.write(txt_data)
+     print(f"txt file {file_path} is created")
+  
+
