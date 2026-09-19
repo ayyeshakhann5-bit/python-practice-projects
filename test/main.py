8d62254f7ce2b17dedac70c621ef9c20797c70e5
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-09-19 14:54:19 +0530

    file detection

--- test/main.py
+++ test/main.py
@@ -1,8 +1,17 @@
+#Python file detection
+
 import os
 
-file_path="test/test.txt"
-
+file_path="/Users/ayesha.khan/Desktop/test"
 if os.path.exists(file_path):
+    
     print(f"The file {file_path} exists.")
+    
+    if os.path.isfile(file_path):
+        print("That is a file")
+        
+    elif os.path.isdir(file_path):
+        print("That is a directory")    
+    
 else:
-    print(f"Th file {file_path} doesn't existss..............")
+    print(f"The file {file_path} doesn't exist.")
