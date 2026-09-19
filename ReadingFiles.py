4f0960ce3f1d6d220e57d8abe828a8886dbaef78
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-09-20 01:12:21 +0530

    ReadingFiles

--- ReadingFiles.py
+++ ReadingFiles.py
@@ -1,11 +1,18 @@
 #Python Reading files (.txt,.json,.csv)
 
+#import json
+
+import csv
+
 file_path="/Users/ayesha.khan/Desktop/output.txt"
 
 try:
   with open(file_path,"r") as file:
-   content=file.read()
-   print(content)
+   #content=file.read()
+    content=csv.reader(file)
+    for line in content:
+      print(line[1])
+    #print(content[1]["Name"])
   
 except FileNotFoundError:
     print("File not found")
