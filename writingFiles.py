36c4ebe01a82585b1cee44758ab0cd8e126481f5
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-09-19 15:17:51 +0530

    writingFiles

--- writingFiles.py
+++ writingFiles.py
@@ -3,7 +3,8 @@
 
 txt_data='I like Pizza'
 
-file_path='output.txt'
+#file_path='output.txt'
+file_path='/Users/ayesha.khan/Desktop/output.txt'
 
 with open(file_path, "w") as file:
      file.write(txt_data)
