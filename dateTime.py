b7bf2572680dd55831309e0585923ce2f63974ba
Ayesha Khan <ayyeshakhann5@gmail.com>
2026-09-20 11:56:57 +0530

    DateTime

--- dateTime.py
+++ dateTime.py
@@ -5,4 +5,13 @@ today=datetime.date.today()
 time=datetime.time(12,30,0)
 now=datetime.datetime.now()
 now=now.strftime("%H:%M:%S %d-%m-%Y")
-print(now)
+#print(now)
+
+target_datetime=datetime.datetime(2020,1,2,12,30,0)
+
+current_datetime=datetime.datetime.now()
+
+if(current_datetime>target_datetime):
+  print("Target date already passed")
+else:
+  print("Target date not already passed")  
